dcf52701d60018ef22dc75d53e5966b2efdf863a
Jon Major <jonmajor@gmail.com>
2015-07-16 18:07:41 +0000

    Set force_login to true, refactor actions.rb and add login/logout links to home page

--- app/actions.rb
+++ app/actions.rb
@@ -1,47 +1,41 @@
-# Homepage (Root path)
-require 'sinatra'
-
 configure do
   enable :sessions
 end
 
-use OmniAuth::Builder do
-  provider :twitter, 'jBlLVqrNSn7BY48FQFcVu9DjY', 'Y3hByR3vvFUSlXFouU5xC2eUeoGiqO2TTXJWeu1tUCODyFUK4f'
-end
+# Homepage (Root path)
 
 get '/' do
   erb :index
 end
- 
-helpers do
-  def admin?
-    session[:admin]
-  end
-end
- 
-get '/public' do
-  "This is the public page - everybody is welcome!"
-end
- 
-get '/private' do
-  halt(401,'Not Authorized') unless admin?
-  "This is the private page - members only"
-end
+
+
 
 get '/login' do
-  redirect to("/auth/twitter")
+  redirect to("/auth/twitter?force_login=true")
 end
 
 get '/auth/twitter/callback' do
-  env['omniauth.auth'] ? session[:admin] = true : halt(401,'Not Authorized')
-  "You are now logged in"
+  if env['omniauth.auth']
+    session[:user] = request.env['omniauth.auth']['info']['name']
+    redirect to ("/user")
+  else
+    halt(401,'Not Authorized')
+  end
 end
 
 get '/auth/failure' do
   params[:message]
 end
 
+get '/user' do
+  if session[:user]
+    erb :'user/index'
+  else
+    redirect to ("/login")
+  end
+end
+
 get '/logout' do
-  session[:admin] = nil
-  "You are now logged out"
+  session.clear
+  redirect to ("/")
 end
